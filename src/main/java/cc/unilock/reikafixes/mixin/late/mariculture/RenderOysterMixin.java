8dd633930d4fbec17fbb3bc01e3a36bdd292ede2
unilock <unilock@fennet.rentals>
2024-08-16 22:37:29 -0400

    maybe fix RenderOysterMixin
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/reikafixes/mixin/late/mariculture/RenderOysterMixin.java
+++ src/main/java/cc/unilock/reikafixes/mixin/late/mariculture/RenderOysterMixin.java
@@ -1,8 +1,7 @@
 package cc.unilock.reikafixes.mixin.late.mariculture;
 
-import com.llamalad7.mixinextras.sugar.Local;
+import mariculture.core.render.RenderBase;
 import mariculture.core.render.RenderOyster;
-import mariculture.core.tile.TileOyster;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,9 +9,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = RenderOyster.class, remap = false)
 public class RenderOysterMixin {
-    @Inject(method = "init", at = @At(value = "INVOKE", target = "Lmariculture/core/tile/TileOyster;getStackInSlot(I)Lnet/minecraft/item/ItemStack;"), cancellable = true)
-    private void init$getStackInSlot(CallbackInfo ci, @Local TileOyster tile) {
-        if (tile == null) {
+    @Inject(method = "init", at = @At("HEAD"), cancellable = true)
+    private void init$getStackInSlot(CallbackInfo ci) {
+        RenderBase that = (RenderBase) (Object) this;
+        if (that.world.getTileEntity(that.x, that.y, that.z) == null) {
             ci.cancel();
         }
     }
